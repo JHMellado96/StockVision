b6d8ff383b3da587a23ecd0652064d31a40047ca
JHMellado96 <jh.mellado@duocuc.cl>
2024-06-15 14:40:43 -0400

    prueba 4 heroku

--- env/sysbodega/sysbodega/settings.py
+++ env/sysbodega/sysbodega/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-378@0p7fy7hipwrrsw#k8a)yc7fvvhy@ebg)*=eujc7_nj#vw9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['stockvision.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sysbodega.urls'
